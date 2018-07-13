347c57fc71393ce2734d31cbbeafc23d4e7075d3
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-12 19:10:06 -0700

    Create RandomPrograms.bat

---
+++ RandomPrograms.bat
@@ -0,0 +1,13 @@
+@echo off
+title Random Sites Payload (Tarty)
+:start
+timeout %random%
+start notepad.exe
+timeout %random%
+start calc.exe
+timeout %random%
+start iexplore.exe
+timeout %random%
+mspaint.exe
+timeout %random%
+goto start
